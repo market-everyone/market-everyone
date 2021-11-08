e764186ceee6532cbd4d18059ffb474b899e2c05
moduhan <devduhanmo@gmail.com>
2021-11-08 23:11:15 +0900

    feat: 프로젝트 세팅 HTML -> Thymeleaf

---
+++ src/main/java/org/pnm/marketeveryone/MarketEveryoneApplication.java
@@ -0,0 +1,13 @@
+package org.pnm.marketeveryone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketEveryoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketEveryoneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/pnm/marketeveryone/MarketEveryoneApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pnm.marketeveryone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketEveryoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
