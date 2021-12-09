7282989290c669f5d8a8216258891fab1957b2c9
moduhan <devduhanmo@gmail.com>
2021-12-10 05:37:30 +0900

    feat: BaseEntity 생성

--- module-web/src/main/java/web/WebApplication.java
+++ module-web/src/main/java/web/WebApplication.java
@@ -2,8 +2,10 @@ package web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class WebApplication {
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class, args);
---
+++ module-web/src/main/java/web/common/entity/BaseEntity.java
@@ -0,0 +1,23 @@
+package web.common.entity;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(value = {AuditingEntityListener.class})
+public class BaseEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime regDate;
+
+    @LastModifiedDate
+    @Column
+    private LocalDateTime modDate;
+}
