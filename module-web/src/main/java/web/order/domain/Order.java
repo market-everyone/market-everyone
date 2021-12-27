9401f6ec2b728db51e4cdda900debde474e72c43
Sunny <ishong@inu.ac.kr>
2021-12-27 20:00:52 +0900

    feat: order 기능 도메인 추가

--- module-web/src/main/java/web/order/domain/Order.java
+++ module-web/src/main/java/web/order/domain/Order.java
@@ -1,11 +1,62 @@
 package web.order.domain;
 
+import lombok.*;
+import web.item.domain.Item;
+import web.order.domain.item.OrderItem;
+import web.user.domain.User;
+
 import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Table(name = "orders")
 @Entity
+@Getter @Setter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class Order {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ORDER_NO")
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "USER_NO")
+    private User user;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+
+    private LocalDateTime date;
+
+    @Builder.Default
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    public void addOrderItem(OrderItem orderItem){
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public static Order createOrder(User user, Item item, OrderItem... orderItems){
+        Order order = new Order();
+        order.setUser(user);
+        order.setStatus(OrderStatus.ORDER);
+        order.setDate(LocalDateTime.now());
+        for(OrderItem orderItem : orderItems){
+            order.addOrderItem(orderItem);
+        }
+        return order;
+    }
+
+    public int getTotalPrice(){
+        int totalPrice = 0;
+        for(OrderItem orderItem : orderItems){
+            totalPrice += orderItem.getTotalPrice();
+        }
+        return totalPrice;
+    }
 }
--- module-web/src/main/java/web/order/domain/item/OrderItem.java
+++ module-web/src/main/java/web/order/domain/item/OrderItem.java
@@ -1,13 +1,49 @@
 package web.order.domain.item;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import lombok.*;
+import web.item.domain.Item;
+import web.order.domain.Order;
+
+import javax.persistence.*;
 
 @Entity
+@Getter @Setter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class OrderItem {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ORDER_ITEM_NO")
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ITEM_NO")
+    private Item item;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ORDER_NO")
+    private Order order;
+
+    private int count;
+    private int price;
+
+    public static OrderItem createOrderItem(Item item, int price, int count){
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setPrice(price);
+        orderItem.setCount(count);
+
+//        item.removeStock(count); // 재고 감소 (예정)
+        return orderItem;
+    }
+
+//
+//    public void cancel(){
+//        getItem().add
+//    }
+
+    public int getTotalPrice(){
+        return getPrice() * getCount();
+    }
 }
