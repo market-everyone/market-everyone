37d07cfa490e62ce6e1c82ae87f89b58440a6666
robolovo <dlwnstj21@naver.com>
2022-01-03 13:29:00 +0900

    feat: 리뷰 API 구현

--- module-web/src/main/java/web/review/controller/dto/request/ReviewSaveRequest.java
+++ module-web/src/main/java/web/review/controller/dto/request/ReviewSaveRequest.java
@@ -1,4 +1,22 @@
 package web.review.controller.dto.request;
 
+import lombok.*;
+import web.review.domain.Review;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 public class ReviewSaveRequest {
+    private String title;
+    private String content;
+    private int star;
+
+    public Review toReview() {
+        return Review.builder()
+                .title(title)
+                .content(content)
+                .star(star)
+                .build();
+    }
 }
