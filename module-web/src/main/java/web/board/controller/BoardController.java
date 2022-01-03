14767c0f727567946fa00cc87d4ed07df9ec6ee6
robolovo <dlwnstj21@naver.com>
2022-01-03 13:29:54 +0900

    feat: 고객센터 API 구현

--- module-web/src/main/java/web/board/controller/BoardController.java
+++ module-web/src/main/java/web/board/controller/BoardController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import web.board.controller.dto.BoardResponseDTO;
 import web.board.domain.Board;
@@ -19,8 +20,6 @@ public class BoardController {
 
     private final BoardService boardService;
 
-
-
     @GetMapping("/notice")
     public String noticePage(PageRequestDTO pageRequestDTO, Model model) {
         model.addAttribute("result", boardService.getList(Type.NOTICE, pageRequestDTO));
@@ -31,5 +30,19 @@ public class BoardController {
     public String faqPage(PageRequestDTO pageRequestDTO, Model model) {
         model.addAttribute("result", boardService.getList(Type.FAQ, pageRequestDTO));
         return "service/faq";
+    }
+
+    @GetMapping("/notice/{id}")
+    public String noticeDetailPage(@PathVariable Long id,
+                                   Model model) {
+        model.addAttribute("notice", boardService.findById(id));
+        return "service/noticeDetail";
+    }
+
+    @GetMapping("/faq/{id}")
+    public String faqDetailPage(@PathVariable Long id,
+                                Model model) {
+        model.addAttribute("faq", boardService.findById(id));
+        return "service/faqDetail";
     }
 }
